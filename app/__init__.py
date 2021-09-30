0184270fdf5210082c2cc03e3973c1219b7fb25e
Ernest <ekedesh@gmail.com>
2021-09-30 18:24:43 +0300

    [R]: init file

--- app/__init__.py
+++ app/__init__.py
@@ -15,11 +15,12 @@ def create_app(environment="development"):
     from config import config
     from app.views import (
         main_blueprint,
+        client_blueprint,
         auth_blueprint,
     )
     from app.models import (
         User,
-        AnonymousUser,
+        Client,
     )
 
     # Instantiate app.
@@ -32,24 +33,24 @@ def create_app(environment="development"):
 
     # Set up extensions.
     db.init_app(app)
-    login_manager.init_app(app)
+    # login_manager.init_app(app)
 
     # Register blueprints.
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
-
+    app.register_blueprint(client_blueprint)
     # Set up flask login.
-    @login_manager.user_loader
-    def get_user(id):
-        return User.query.get(int(id))
+    # @login_manager.user_loader
+    # def get_user(id):
+    #     return User.query.get(int(id))
 
-    login_manager.login_view = "auth.login"
-    login_manager.login_message_category = "info"
-    login_manager.anonymous_user = AnonymousUser
+    # login_manager.login_view = "auth.login"
+    # login_manager.login_message_category = "info"
+    # login_manager.anonymous_user = AnonymousUser
 
     # Error handlers.
-    @app.errorhandler(HTTPException)
-    def handle_http_error(exc):
-        return render_template("error.html", error=exc), exc.code
+    # @app.errorhandler(HTTPException)
+    # def handle_http_error(exc):
+    #     return render_template("error.html", error=exc), exc.code
 
     return app
