2015a20f2fb17caec27ca75c09a92ea846f0a07c
Ernest <ekedesh@gmail.com>
2021-09-30 18:30:00 +0300

    [A]: pydantic model for client

---
+++ app/models/client_model.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from pydantic import BaseModel
+from datetime import datetime
+from pydantic.networks import EmailStr
+
+
+from .client import(
+    MaritalStatus,
+)
+
+class ClientModel(BaseModel):
+    name: str
+    username: str
+    email: EmailStr
+    phone_number: str
+    address: str
+    zip_plus_town: str
+    nationality: str
+    birthday: datetime
+    profession: str
+    guaranteed_solution: str
+    type_of_save: str
+    marital_status: MaritalStatus
+    smoking: str
+    sex: str
