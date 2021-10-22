2b9287aa8f9f4367d82fc1f25acb011a95c5cf75
Ernest <ekedesh@gmail.com>
2021-10-22 16:34:02 +0300

    [R]: send mail service

--- app/controllers/send_email.py
+++ app/controllers/send_email.py
@@ -1,16 +1,16 @@
 from app.logger import log
 from config import BaseConfig as cfg
 # from flask import render_template
-from flask_mail import Mail, Message
-from flask import current_app
+from flask_mail import Message
 
 from app.models import Client
+from app import mail
 
 
 def send_mail(client: Client):
-    mail = Mail(current_app)
     # letter_text = render_template("contact_email.html", client=client)
+    log(log.INFO, "Send mail server: %s:%d", cfg.MAIL_SERVER, cfg.MAIL_PORT)
     msg = Message("Samme", sender=cfg.MAIL_FROM, recipients=client.email)
     msg.body = "Hello world"
-    log(log.INFO, "Message was sent to %s", client.email)
     mail.send(msg)
+    log(log.INFO, "Message was sent to %s", client.email)
