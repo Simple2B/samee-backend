8b242d1d65b30263acc827ec703efc108a801548
Ernest <ekedesh@gmail.com>
2021-10-22 14:48:09 +0300

    [A]: files to send mail

--- app/controllers/send_email.py
+++ app/controllers/send_email.py
@@ -1,35 +1,15 @@
-import smtplib
+from logger import log
+from config import BaseConfig as cfg
+# from flask import render_template
+from flask_mail import Mail, Message
 
-from app.logger import log
-from config import BaseConfig
-
-
-GMAIL_USER = BaseConfig.GMAIL_USER
-GMAIL_PASSWORD = BaseConfig.GMAIL_PASSWORD
+from app.models import Client
 
 
-def send_email(receiver):
-    sent_from = GMAIL_USER
-    to = receiver
-    subject = 'Lorem ipsum dolor sit amet'
-    body = 'consectetur adipiscing elit'
-
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
-
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
-
-    try:
-        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        smtp_server.ehlo()
-        smtp_server.login(GMAIL_USER, GMAIL_PASSWORD)
-        smtp_server.sendmail(sent_from, to, email_text)
-        smtp_server.close()
-        log(log.INFO, "Email sent successfully!")
-        # print("Email sent successfully!")
-    except Exception as ex:
-        log(log.ERROR, "Something went wrong: [%s] ", ex)
-        # print("Something went wrong: ", ex)
+def send_mail(client: Client):
+    mail = Mail
+    # letter_text = render_template("contact_email.html", client=client)
+    msg = Message("Samme", sender=cfg.MAIL_FROM, recipients=client.email)
+    msg.body = "Hello world"
+    log(log.INFO, "Message was sent to %s", client.email)
+    mail.send(msg)
